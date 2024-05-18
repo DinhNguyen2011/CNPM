308ae9c1b87468c450798ac261d8f3733fccc94d
Phat Truong Quang <tqphat31082k3@gmail.com>
2024-05-18 17:56:42 +0700

    extra error

--- WindowsFormsApp1/WindowsFormsApp1/GUI/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/GUI/Form1.Designer.cs
@@ -272,7 +272,6 @@ namespace WindowsFormsApp1
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.guna2Panel1.ResumeLayout(false);
             this.guna2Panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
