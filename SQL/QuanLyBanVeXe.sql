99cb4c8c92441f7ef685ced7ffd1064a2d1c6cd6
ChiDuck <chiducocelot@gmail.com>
2024-05-21 23:56:03 +0700

    UPDATE PROC HUYVEXE

--- SQL/QuanLyBanVeXe.sql
+++ SQL/QuanLyBanVeXe.sql
@@ -817,26 +817,13 @@ GO
 CREATE OR ALTER PROC HUYVEXE @mave int 
 AS
 BEGIN
-	DELETE VEXE WHERE MAVE = @mave
-	DELETE CHITIETVEXE WHERE MACTVX = @mave
+	UPDATE VEXE SET MACTVX = NULL WHERE MAVE = @mave
+	DELETE CHITIETVEXE WHERE MAVE = @mave
+	--DELETE VEXE WHERE MAVE = @mave
+	
 END
-
+EXEC HUYVEXE 2
 GO
-/*==============================================================*/
-/* Stored procedure: Sửa chi tiết vé        			        */
-/*==============================================================*/
-CREATE OR ALTER PROC SUACHITIETVEXE @mave int, @giodi datetime, @gioden datetime,
-@vitrighe nvarchar(5), @giave money, @trangthai nvarchar(20)
-AS
-BEGIN
-	IF (@giodi < GETDATE()) RETURN N'Giờ đi không hợp lệ'  
-	IF (@giodi >= @gioden) RETURN N'Giờ đến không được bé hơn hoặc bằng giờ đi'
-	UPDATE CHITIETVEXE 
-	SET GIODI = @giodi, GIODEN = @gioden, VITRIGHE = @vitrighe, GIAVE = @giave, TRANGTHAI = @trangthai
-	WHERE MAVE = @mave
-END
-
-go
 
 CREATE OR ALTER PROC THEMVE @tenve nvarchar(50), @ghichu nvarchar(50), @machuyen int, @makh int, 
 @ghe char(2), @trangthai nvarchar(20) 
